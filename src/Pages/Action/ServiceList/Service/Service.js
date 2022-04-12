8d4e2e0141baa6951221ab12217efff179a95b82
Rahman Rakib <rahmanrakibur6589@gmail.com>
2022-04-13 01:45:11 +0600

    delete method issue solved

--- src/Pages/Action/ServiceList/Service/Service.js
+++ src/Pages/Action/ServiceList/Service/Service.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Col, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import '../../../../App.css'
 
 const Service = ({ x }) => {
-     const { photo, roomTitle, bedType, description, price, bedCapacity, _id } = x;
+     const { photo, roomTitle, bedType, description, price, bedCapacity, _id, room, setRoom } = x;
 
      const navigate = useNavigate();
      const handleNavigate = editID => {
           navigate(`/edit/${editID}`)
+     }
+
+     //handle delete service
+     const handleDeleteService = (deleteId) => {
+          fetch(`http://localhost:5000/rooms/${deleteId}`, {
+               method: 'DELETE',
+          })
+               .then(res => res.json())
+               .then(data => {
+                    if (data.deletedCount > 0) {
+                         alert('Service Deleted Successfully');
+                         window.location.reload();
+                    }
+               })
      }
 
      return (
@@ -21,7 +35,7 @@ const Service = ({ x }) => {
                               {description}
                          </Card.Text>
                          <Button variant="primary" onClick={() => handleNavigate(x._id)}>Edit</Button>
-                         <Button variant="danger">Delete</Button>
+                         <Button variant="danger" onClick={() => handleDeleteService(x._id)}>Delete</Button>
                     </Card.Body>
                </Card>
           </Col>
